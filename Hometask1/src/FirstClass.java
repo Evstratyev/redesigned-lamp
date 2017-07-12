aa748d58cd0844e7b5e27160ad44f5cdd92570ce
Evstratyev <orin1111@mail.ru>
2017-07-12 22:58:09 +0300

    Add files via upload

---
+++ Hometask1/src/FirstClass.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+/**
+ * Created by Евген on 07.07.2017.
+ */
+public class FirstClass {
+    public static void main (String [] args) {
+//
+//        double firstVar = 1;                          //ввод данных через присвоение из проги и вывод результа
+//        double secondVar = 5;
+//        double xVar;
+//        xVar = -secondVar/firstVar;
+//        System.out.println("x = "+xVar);
+
+
+
+//        double firstVar = 2;                          //после ввода значений вывод осуществляется с введенными переменными, а потом сам "х"
+//        double secondVar = -3;
+//        System.out.println("решение уравнения ax + b = 0");
+//        System.out.println("a = "+firstVar +"\t"+"b = "+secondVar );
+//        System.out.format("%f" +"x" +"%+f" +" = 0%n",firstVar,secondVar);
+//        double xVar;
+//        xVar = -secondVar/firstVar;
+//        System.out.println("x = "+xVar);
+
+
+
+//        double firstVar = -97.7;                      //условие если х = бесконечность
+//        double secondVar = 24;
+//        if (firstVar==0){
+//            System.out.println("еще слишком рано знать как делить на 0");
+//        } else {
+//            double xVar;
+//            xVar = -secondVar / firstVar;
+//            System.out.println("x = " + xVar);
+//        }
+
+
+
+//        System.out.println("Решение уравнения ах + b = 0");     //ввод значений с клавиатуры
+//        System.out.print("Введите значение переменной а: ");
+//        double aVar;
+//        Scanner sc = new Scanner(System.in);
+//        aVar = sc.nextDouble();
+//
+//        System.out.print("Введите значение переменной b: ");
+//        double bVar;
+//        bVar = sc.nextDouble();
+//
+//        System.out.format("%f" +"x" +"%+f" +" = 0%n",aVar,bVar);
+//
+//        Double xVar;
+//        if (aVar == 0){
+//            System.out.print("Деление на ноль");
+//        }else{
+//        xVar = -bVar/aVar;
+//
+//        System.out.print("x = "+xVar);
+//        }
+    }
+}
+
